7bac94de3dbb07bfef57434c5ca4f5ff76bd823d
Richard Diphoorn <rdiphoorn@users.noreply.github.com>
2015-05-30 21:11:55 +0200

    Create Snippet-Comments.ps1

---
+++ Snippet-Comments.ps1
@@ -0,0 +1,30 @@
+function your-function {
+<#
+.SYNOPSIS
+A brief description of the function or script.
+
+.DESCRIPTION
+A longer description.
+
+.PARAMETER FirstParameter
+Description of each of the parameters
+
+.PARAMETER SecondParameter
+Description of each of the parameters
+
+.INPUTS
+Description of objects that can be piped to the script
+
+.OUTPUTS
+Description of objects that are output by the script
+
+.EXAMPLE
+Example of how to run the script
+
+.LINK
+Links to further documentation
+
+.NOTES
+Detail on what the script does, if this is needed
+
+#>
